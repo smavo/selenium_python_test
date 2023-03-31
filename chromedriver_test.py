064df2f3197a42f32049c3bb0b5f470323aa4ffe
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-03-30 21:45:15 -0500

    Subiendo proyecto al repositorio, primeras configuraciones

---
+++ chromedriver_test.py
@@ -0,0 +1,19 @@
+# Importar la libreria de webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+# Maximizar el navegador
+driver.maximize_window()
+
+print("Bienvenido a Pycharm")
+
+# Acceder a la url https://demoqa.com/
+driver.get("https://www.saucedemo.com/")
+
+# Imprimir
+print(driver.title)
+
+# Finalizar el driver
+driver.close()
