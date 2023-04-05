8421b035c103af6f2b3832f5119eb70cff40d300
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-05 11:49:07 -0500

    Test activando los  options - headless

--- 02_Test_EdgeOptions_headless.py
+++ 02_Test_EdgeOptions_headless.py
@@ -1,24 +1,26 @@
 # Importar la libreria de webdriver
 import time
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-# service = Service(executable_path=".\Drivers\chromedriver.exe")
-service = Service(executable_path="./Drivers/chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-driver.set_page_load_timeout(5)
+options = EdgeOptions()
+options.add_argument("--headless")
+driver = webdriver.Edge(options=options)
+# driver.set_page_load_timeout(2)
 
 # Maximizar el navegador
-driver.maximize_window()
+# driver.maximize_window()
 
 # Acceder al sitio
 driver.get("https://google.com")
-
+print(driver.title)
 driver.find_element(By.NAME, "q").send_keys("Smavodev.com")
 driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
 time.sleep(2)
+print(driver.title)
 driver.close()
 driver.quit()
+print("Test Completed Edge")
+
