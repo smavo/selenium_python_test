a22b257fb7a8e8e610cc83d2ad7bbbf806319b78
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-05 11:31:50 -0500

    Primer Test

---
+++ 01_FirstTest.py
@@ -0,0 +1,24 @@
+# Importar la libreria de webdriver
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# service = Service(executable_path=".\Drivers\chromedriver.exe")
+service = Service(executable_path="./Drivers/chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.set_page_load_timeout(5)
+
+# Maximizar el navegador
+driver.maximize_window()
+
+# Acceder al sitio
+driver.get("https://google.com")
+
+driver.find_element(By.NAME, "q").send_keys("Automation step by step")
+driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
+time.sleep(2)
+driver.close()
+driver.quit()
