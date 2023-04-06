d76431a075c028d3c0ef6fb661ba1d5de7fb161d
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-06 12:58:22 -0500

    Ordenando los pruebas por carpetas

--- Test Examples/01_chromedriver_local_test.py
+++ Test Examples/01_chromedriver_local_test.py
@@ -1,15 +1,15 @@
 # Importar la libreria de webdriver
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.chrome.service import Service
 
 # service = Service(executable_path=".\Drivers\chromedriver.exe")
-service = Service(executable_path="./Drivers/msedgedriver.exe")
-driver = webdriver.Edge(service=service)
+service = Service(executable_path="../Drivers/chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 
 # Maximizar el navegador
 driver.maximize_window()
 
-print("Bienvenido a Pycharm desde Edge")
+print("Bienvenido a Pycharm desde Chrome")
 
 # Acceder a la url
 driver.get("https://www.saucedemo.com/")
--- Test Examples/01_edgeDriver_local_test.py
+++ Test Examples/01_edgeDriver_local_test.py
@@ -1,15 +1,15 @@
 # Importar la libreria de webdriver
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
 
 # service = Service(executable_path=".\Drivers\chromedriver.exe")
-service = Service(executable_path="./Drivers/chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+service = Service(executable_path="../Drivers/msedgedriver.exe")
+driver = webdriver.Edge(service=service)
 
 # Maximizar el navegador
 driver.maximize_window()
 
-print("Bienvenido a Pycharm desde Chrome")
+print("Bienvenido a Pycharm desde Edge")
 
 # Acceder a la url
 driver.get("https://www.saucedemo.com/")
