9056dfea600f4c918389c3fc0ff019b4aaff1777
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-06 13:41:41 -0500

    Agregando Funciones y testsample de ejemplo

--- POM_2/Funciones/Funciones.py
+++ POM_2/Funciones/Funciones.py
@@ -1,3 +1,6 @@
+import time
+from selenium.webdriver.common.by import By
+
 
 class Funciones_Globales:
 
@@ -6,3 +9,33 @@ class Funciones_Globales:
 
     def saludos(self):
         print("Welcome a Page Object Model")
+
+    def Tiempo_sleep(self, tiempo):
+        t = time.sleep(tiempo)
+        return t
+
+    def Navegar(self, URL):
+        self.driver.get(URL)
+        self.driver.maximize_window()
+
+    def Navegar_T(self, URL, Tiempo):
+        self.driver.get(URL)
+        self.driver.maximize_window()
+        t = time.sleep(Tiempo)
+        return t
+
+    def Texto_XPath(self, xpath, texto, tiempo):
+        value = self.driver.find_element(By.XPATH, xpath)
+        value.send_keys(texto)
+        t = time.sleep(tiempo)
+        return t
+
+    def Texto_ID(self, ID, texto, tiempo):
+        value = self.driver.find_element(By.ID, ID)
+        value.send_keys(texto)
+        t = time.sleep(tiempo)
+        return t
+
+    def Button_ID(self, value):
+        value = self.driver.find_element(By.ID, value)
+        value.click()
