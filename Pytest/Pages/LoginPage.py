81ba785c0969b8d172b022d09f36533d19b0899e
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-09 15:20:56 -0500

    Test usando Fixture y Mark

--- Pytest/Pages/LoginPage.py
+++ Pytest/Pages/LoginPage.py
@@ -28,4 +28,23 @@ class LoginPage:
         f.Button_TYPE("xpath", "//a[@class='nav-link'][contains(.,'Logout')]", 1)
         f.cerrar_test()
 
-    # def Modulo_System_Inf(self):
+    def Login_Test_II(self, USERNAME, PASSWORD, TIEMPO):
+        driver = self.driver
+        f = Funciones_Globales_Pyest(driver)
+
+        f.Texto_TYPE("name", "username", USERNAME, TIEMPO)
+        f.Texto_TYPE("name", "password", PASSWORD, TIEMPO)
+        f.Button_TYPE("xpath", "//button[@type='submit'][contains(.,'Login')]", 1)
+        f.Tiempo_sleep(3)
+
+    def Logout_Test_II(self):
+        driver = self.driver
+        f = Funciones_Globales_Pyest(driver)
+        f.Button_TYPE("xpath", "//span[@class='oxd-userdropdown-tab']", 1)
+        f.Button_TYPE("xpath", "//a[contains(.,'Logout')]", 2)
+        f.cerrar_test()
+
+    def Modulo_Seleccionado(self, menu):
+        driver = self.driver
+        f = Funciones_Globales_Pyest(driver)
+        f.Button_TYPE("xpath", "//a[contains(.,'"+menu+"')]", 2)
